49aabe0b89f4a730a0249062a0be9f319b1dc5bf
xiaomaomiao <1051346747@qq.com>
2022-03-11 08:37:56 +0800

    init

---
+++ src/main/java/com/ssy/uav_dispatch/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.ssy.uav_dispatch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author miaomiao
+ * @date 2022/3/10 22:58
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/ssy/uav_dispatch/UavDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssy.uav_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UavDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
